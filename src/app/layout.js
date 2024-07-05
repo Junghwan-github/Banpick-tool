cb2736f7f804aea25068cf09fb5514296cd43863
junghwan <j2studio.jungie2@gmail.com>
2024-07-05 22:57:38 +0900

    feature / adfit modify

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import "./styles/global.css";
 import Header from "./layout/header";
 import Footer from "./layout/footer";
-import Script from "next/script";
 import KakaoAdfit from "./components/kakao/kakao";
 
 export const metadata = {
@@ -12,7 +11,7 @@ export const metadata = {
   ],
   title: "밴픽 툴 - 리그오브레전드",
   description:
-    "롤 밴픽 은 리그 오브 레전드(LoL) 게임에서 밴픽 전략을 최적화하기 위해 개발된 도구입니다. 이 시뮬레이터는 상대팀보다 유리한 밴픽 전략을 세우는 데 도움을 주며, 인게임 내 밴픽 과정에서의 불편함을 해소합니다.",
+    "밴픽툴 은 LoL 게임에서 밴픽 전략을 최적화하기 위해 개발된 도구로, 싱글 모드와 매치 모드를 지원합니다. 지금 바로 상대보다 유리한 전략을 세워 보세요.",
   keywords: [
     "롤",
     "리그오브레전드",
@@ -22,6 +21,14 @@ export const metadata = {
     "밴픽 툴",
     "밴픽 시뮬레이터",
     "밴픽 연습",
+    "밴픽툴",
+    "banpick tool",
+    "banpicktool",
+    "pick ban",
+    "pickbantool",
+    "픽밴",
+    "픽밴툴",
+    "밴픽 시뮬",
   ],
   robots: "index, follow",
   openGraph: {
@@ -55,13 +62,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <head>
-        <Script
-          id="kakao-adfit-sdk"
-          src="//t1.daumcdn.net/kas/static/ba.min.js"
-          strategy="beforeInteractive"
-        />
-      </head>
       <body>
         <div id="ban-pick">
           <Header />
